fa319c44769db7b49841f672d33d08daa785d5f7
Monica He <carpediemkai@yahoo.com>
2014-12-22 21:53:55 -0500

    Assessment for Week 1 - structure only

---
+++ Classwork/Week 2/Week 1 Assessment/Week 1 Assessment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week 1 Assessment
+//
+//  Created by Monica He on 12/22/14.
+//  Copyright (c) 2014 Monica He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
