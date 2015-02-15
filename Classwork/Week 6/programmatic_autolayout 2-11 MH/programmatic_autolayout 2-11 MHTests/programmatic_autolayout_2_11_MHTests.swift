468acbde42a6a0daf06a933587c265d1da354eb7
Monica He <carpediemkai@yahoo.com>
2015-02-15 14:36:28 -0500

    Committing on 2-15 before Design Class

---
+++ Classwork/Week 6/programmatic_autolayout 2-11 MH/programmatic_autolayout 2-11 MHTests/programmatic_autolayout_2_11_MHTests.swift
@@ -0,0 +1,36 @@
+//
+//  programmatic_autolayout_2_11_MHTests.swift
+//  programmatic_autolayout 2-11 MHTests
+//
+//  Created by Monica He on 2/11/15.
+//  Copyright (c) 2015 Monica He. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class programmatic_autolayout_2_11_MHTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
